38822a7e06c174b5693c48bc68487e9b0395aff8
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-07-15 15:08:16 +0530

    applying consistency over the web app

---
+++ src/core/eventBus.ts
@@ -0,0 +1,28 @@
+class EventBus {
+  private listeners: Record<string, Function[]> = {};
+
+  on(event: string, callback: Function) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: Function) {
+    if (!this.listeners[event]) return;
+    this.listeners[event] = this.listeners[event].filter(cb => cb !== callback);
+  }
+
+  emit(event: string, data?: any) {
+    if (!this.listeners[event]) return;
+    this.listeners[event].forEach(callback => {
+      try {
+        callback(data);
+      } catch (err) {
+        console.error(`Error in event listener for ${event}:`, err);
+      }
+    });
+  }
+}
+
+export const eventBus = new EventBus();
